beaac69d3c3aea0e681e9ca26d8d24d45c44d30f
mudmen72 <64137332+mudmen72@users.noreply.github.com>
2024-11-18 22:52:08 +0100

    Update config.js
    weatherAPI

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '8094bc5990fac9d0623a97c189377a22', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
