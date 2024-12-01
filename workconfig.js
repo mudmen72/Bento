cfb6dd56a45afa050541118932c7e57bb0241da1
mudmen72 <64137332+mudmen72@users.noreply.github.com>
2024-12-01 10:14:58 +0100

    Create workconfig.js
    added js for work

---
+++ workconfig.js
@@ -0,0 +1,230 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+	// ├┴┐├─┤└─┐││  └─┐
+	// └─┘┴ ┴└─┘┴└─┘└─┘
+
+	// General
+	name: 'mudmen',
+	imageBackground: true,
+	openInNewTab: true,
+	twelveHourFormat: false,
+
+	// Greetings
+	greetingMorning: 'Go to work!',
+	greetingAfternoon: 'Good afternoon,',
+	greetingEvening: 'Good evening,',
+	greetingNight: 'Go to Sleep u fucker!',
+
+	// Layout
+	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+
+	// Weather
+	weatherKey: '8094bc5990fac9d0623a97c189377a22', // Write here your API Key
+	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherUnit: 'C', // 'F', 'C'
+	language: 'en', // More languages in https://openweathermap.org/current#multi
+
+	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '48.853',
+	defaultLongitude: '2.348',
+
+	// Autochange
+	autoChangeTheme: true,
+
+	// Autochange by OS
+	changeThemeByOS: true,
+
+	// Autochange by hour options (24hrs format, string must be in: hh:mm)
+	changeThemeByHour: false,
+	hourDarkThemeActive: '18:30',
+	hourDarkThemeInactive: '07:00',
+
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+			name: 'Github',
+			icon: 'github',
+			link: 'https://github.com/',
+		},
+		{
+			id: '2',
+			name: 'Mail',
+			icon: 'mail',
+			link: 'https://mail.google.com/mail/',
+		},
+		{
+			id: '3',
+			name: 'Torrent',
+			icon: 'download',
+			link: 'http://192.168.1.36:8080/',
+		},
+		{
+			id: '4',
+			name: 'Calendar',
+			icon: 'calendar',
+			link: 'https://calendar.google.com/calendar/r',
+		},
+		{
+			id: '5',
+			name: 'Reddit',
+			icon: 'bot-message-square',
+			link: 'https://reddit.com',
+		},
+		{
+			id: '6',
+			name: 'YouTube',
+			icon: 'youtube',
+			link: 'https://www.youtube.com/',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'headphones',
+			link: 'https://open.spotify.com',
+		},
+		{
+			id: '2',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/',
+		},
+		{
+			id: '3',
+			name: 'bot',
+			icon: 'bot',
+			link: 'https://discord.com/app',
+		},
+		{
+			id: '4',
+			name: 'Amazon',
+			icon: 'shopping-bag',
+			link: 'https://amazon.com/',
+		},
+		{
+			id: '5',
+			name: 'Hashnode',
+			icon: 'pen-tool',
+			link: 'https://hashnode.com/',
+		},
+		{
+			id: '6',
+			name: 'Figma',
+			icon: 'figma',
+			link: 'https://figma.com/',
+		},
+	],
+
+	// ┬  ┬┌─┐┌┬┐┌─┐
+	// │  │└─┐ │ └─┐
+	// ┴─┘┴└─┘ ┴ └─┘
+
+	// First Links Container
+	firstlistsContainer: [
+		{
+			icon: 'music',
+			id: '1',
+			links: [
+				{
+					name: 'Plex',
+					link: 'http://192.168.1.33:32400/web/index.html#!/',
+				},
+				{
+					name: 'Twitch',
+					link: 'https://www.twitch.tv/illojuan',
+				},
+				{
+					name: 'RTvE',
+					link: 'https://www.rtve.es/play/la-revuelta/',
+				},
+				{
+					name: 'Netflix',
+					link: 'https://www.netflix.com/browse',
+				},
+			],
+		},
+		{
+			icon: 'coffee',
+			id: '2',
+			links: [
+				{
+					name: 'Photopea',
+					link: 'https://www.photopea.com/',
+				},
+				{
+					name: 'Meta Business',
+					link: 'https://business.facebook.com/latest/home?asset_id=219132751287682',
+				},
+				{
+					name: 'Google Drive',
+					link: 'https://drive.google.com/drive/u/0/home',
+				},
+				{
+					name: 'Facebook',
+					link: 'http://www.facebook.com/',
+				},
+			],
+		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: 'binary',
+			id: '1',
+			links: [
+				{
+					name: 'Spotify',
+					link: 'https://www.spotify.com',
+				},
+				{
+					name: 'Reddit',
+					link: 'https://www.reddit.com',
+				},
+				{
+					name: 'Hashnode',
+					link: 'https://www.hashnode.com',
+				},
+				{
+					name: 'Pocket',
+					link: 'https://www.pocket.com',
+				},
+			],
+		},
+		{
+			icon: 'github',
+			id: '2',
+			links: [
+				{
+					name: 'Front',
+					link: 'https://www.reddit.com/r/Frontend/',
+				},
+				{
+					name: 'Rust',
+					link: 'https://www.reddit.com/r/rust/',
+				},
+				{
+					name: 'Go',
+					link: 'https://www.reddit.com/r/golang/',
+				},
+				{
+					name: 'Repos',
+					link: 'https://github.com/migueravila',
+				},
+			],
+		},
+	],
+};
