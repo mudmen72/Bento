b29c4843453b7cada87ed281eda9537537c51f9b
mudmen72 <64137332+mudmen72@users.noreply.github.com>
2026-01-05 18:13:42 +0100

    Update time.js
    added weekdays

--- assets/js/time.js
+++ assets/js/time.js
@@ -6,11 +6,13 @@
 window.onload = displayClock();
 function displayClock() {
 	const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+	const weekdayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 	var d = new Date();
 	var mm = monthNames[d.getMonth()];
 	var dd = d.getDate();
-	var min = (mins = ('0' + d.getMinutes()).slice(-2));
+	var wd = weekdayNames[d.getDay()];
+	var min = ('0' + d.getMinutes()).slice(-2);
 	var hh = d.getHours();
 	var ampm = '';
 
@@ -24,6 +26,7 @@ function displayClock() {
 	document.getElementById('separator').innerHTML = ' : ';
 	document.getElementById('minutes').innerText = min + ampm;
 
+	document.getElementById('weekday').innerText = wd;
 	document.getElementById('month').innerText = mm;
 	document.getElementById('day').innerText = dd;
 
